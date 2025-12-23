e3176b8c750127bd48ee1d8c5a6a3bd2ff1ce775
Trishan Wagle <mailtotrishan@gmail.com>
2025-12-23 14:45:28 +0545

    feat: fonts, custom colors, shadcn themes setup.

--- kaarya_frontend/app/(public)/page.tsx
+++ kaarya_frontend/app/(public)/page.tsx
@@ -1,15 +1,20 @@
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
+    <section className="space-y-2">
       <h1>Hello World, I am Landing Page</h1>
-      <Link href="/sign-in" className="text-blue-400">
-        Sign In
-      </Link>{" "}
-      <Link href="/sign-up" className="text-blue-400">
-        Sign Up
-      </Link>
-    </div>
+
+      <div className="space-x-2">
+        <Button>
+          <Link href="/sign-in">Sign In</Link>
+        </Button>
+
+        <Button variant="secondary">
+          <Link href="/sign-up">Sign Up</Link>
+        </Button>
+      </div>
+    </section>
   );
 }
