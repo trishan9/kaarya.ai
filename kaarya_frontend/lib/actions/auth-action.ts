3eeba113a485f95e718c3b45256cf01d3a3a9c4b
Trishan Wagle <mailtotrishan@gmail.com>
2026-01-20 14:10:18 +0545

    feat: created server actions (pure api call) for authentication.

---
+++ kaarya_frontend/lib/actions/auth-action.ts
@@ -0,0 +1,25 @@
+"use server";
+
+import { TSigninSchema, TSignupSchema } from "@/app/(auth)/_schemas";
+import { api } from "../api/axios-instance";
+import { API_URLS } from "../api/endpoints";
+import { deleteSession } from "../session";
+import { redirect } from "next/navigation";
+
+export const authActions = {
+  auth: {
+    signup: async (data: TSignupSchema) => {
+      return await api.post(API_URLS.AUTH.SIGNUP, data);
+    },
+    signin: async (credentials: TSigninSchema) => {
+      return await api.post(API_URLS.AUTH.SIGNIN, credentials);
+    },
+    logout: async () => {
+      await deleteSession();
+      return redirect("/sign-in");
+    },
+    getMe: async () => {
+      return await api.get(API_URLS.AUTH.ME);
+    },
+  },
+};
