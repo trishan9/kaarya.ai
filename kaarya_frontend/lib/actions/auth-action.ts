ea975d482ba9b660d1da55b33106bf73c6ab8573
Trishan Wagle <mailtotrishan@gmail.com>
2026-01-20 19:17:56 +0545

    feat: error handling on server actions

--- kaarya_frontend/lib/actions/auth-action.ts
+++ kaarya_frontend/lib/actions/auth-action.ts
@@ -5,19 +5,44 @@ import { api } from "../api/axios-instance";
 import { API_URLS } from "../api/endpoints";
 import { clearSession } from "../session";
 
-export const authActions = {
-  auth: {
-    signup: async (data: TSignupSchema) => {
-      return await api.post(API_URLS.AUTH.SIGNUP, data);
-    },
-    signin: async (credentials: TSigninSchema) => {
-      return await api.post(API_URLS.AUTH.SIGNIN, credentials);
-    },
-    logout: async () => {
-      return await clearSession();
-    },
-    getMe: async () => {
-      return await api.get(API_URLS.AUTH.ME);
-    },
-  },
-};
+export async function signup(data: TSignupSchema) {
+  try {
+    const response = await api.post(API_URLS.AUTH.SIGNUP, data);
+    return response.data;
+  } catch (error: Error | any) {
+    return {
+      success: false,
+      message: error.message || "Signup failed",
+    };
+  }
+}
+
+export async function signin(credentials: TSigninSchema) {
+  try {
+    const response = await api.post(API_URLS.AUTH.SIGNIN, credentials);
+    return response.data;
+  } catch (error: Error | any) {
+    const errorMessage =
+      error?.response?.data?.message || error.message || "Signin failed";
+    return {
+      success: false,
+      message: errorMessage,
+    };
+  }
+}
+
+export async function logout() {
+  await clearSession();
+}
+
+export async function getMe() {
+  try {
+    const response = await api.get(API_URLS.AUTH.ME);
+    return response.data;
+  } catch (error: Error | any) {
+    return {
+      success: false,
+      message: error.message || "Failed to fetch user data",
+    };
+  }
+}
