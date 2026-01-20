cea07791b260f6cf84d04f55160b339cc2970b07
Trishan Wagle <mailtotrishan@gmail.com>
2026-01-20 14:25:37 +0545

    feat: updated signin hook to use actual action instead of dummy

--- kaarya_frontend/app/(auth)/_hooks/use-sign-in.ts
+++ kaarya_frontend/app/(auth)/_hooks/use-sign-in.ts
@@ -1,15 +1,18 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useTransition } from "react";
 import { useForm } from "react-hook-form";
 import z from "zod";
 import { toast } from "sonner";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { signinSchema } from "../_schemas";
-import { signinAction } from "../_actions/signin";
+import { signinSchema, TSigninSchema } from "../_schemas";
+import { authActions } from "@/lib/actions/auth-action";
+import { createSession } from "@/lib/session";
 
 export const useSignIn = () => {
   const [isPending, startTransition] = useTransition();
+  const router = useRouter();
 
   const form = useForm<z.infer<typeof signinSchema>>({
     resolver: zodResolver(signinSchema),
@@ -19,10 +22,27 @@ export const useSignIn = () => {
     },
   });
 
-  async function onSubmit(data: z.infer<typeof signinSchema>) {
+  async function onSubmit(data: TSigninSchema) {
     startTransition(async () => {
-      await signinAction(data);
-      toast("Logged in succesfully");
+      try {
+        const response = await authActions.auth.signin(data);
+
+        const {
+          message,
+          data: { accessToken },
+        } = response.data;
+
+        await createSession(accessToken);
+        router.refresh();
+
+        toast.success(message);
+      } catch (error: Error | any) {
+        const errorMessage =
+          error?.response?.data?.message ||
+          error.message ||
+          "An unexpected error occurred. Please try again.";
+        toast.error(errorMessage);
+      }
     });
   }
 
