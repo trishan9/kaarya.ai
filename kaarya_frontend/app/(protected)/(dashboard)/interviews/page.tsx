d71916fe0b874fa5a252deef568ea3ed8486c66c
Trishan Wagle <mailtotrishan@gmail.com>
2026-02-10 09:29:22 +0545

    feat: created my interviews page which shows interviews taken and created by me.

---
+++ kaarya_frontend/app/(protected)/(dashboard)/interviews/page.tsx
@@ -0,0 +1,22 @@
+import { DashboardHeader } from "../_components/dashboard-header";
+import { OverviewHeaderActions } from "../overview/_components/overview-header-actions";
+import { MyInterviewsBoard } from "./_components/my-interviews-board";
+import { MyInterviewsHero } from "./_components/my-interviews-hero";
+import { getMyInterviewsPageData } from "./interviews-data";
+
+export default async function MyInterviewsPage() {
+  const myInterviewsData = await getMyInterviewsPageData();
+
+  return (
+    <div className="min-h-svh bg-neutral-100 p-2 sm:p-4 lg:pl-0 lg:p-5">
+      <div className="rounded-xl bg-white sm:rounded-2xl">
+        <DashboardHeader title="My Interviews" actions={<OverviewHeaderActions />} />
+
+        <div className="space-y-6 px-3 pb-6 sm:px-4 sm:pb-8">
+          <MyInterviewsHero {...myInterviewsData.hero} />
+          <MyInterviewsBoard {...myInterviewsData.board} />
+        </div>
+      </div>
+    </div>
+  );
+}
