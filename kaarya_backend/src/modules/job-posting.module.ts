5faccdd3d235fd4df953dadd5cc684e1aaaf168f
Trishan Wagle <mailtotrishan@gmail.com>
2026-02-13 15:15:15 +0545

    feat: add resume service and job services on job module.

--- kaarya_backend/src/modules/job-posting.module.ts
+++ kaarya_backend/src/modules/job-posting.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { JobApplicationController } from 'src/controllers/job-application.controller';
 import { JobPostingController } from 'src/controllers/job-posting.controller';
 import {
   ApplicationSchema,
@@ -9,6 +10,7 @@ import {
   JobPostingSchema,
   JobPostingSchemaClass,
 } from 'src/entities/job-posting.schema';
+import { ResumeSchema, ResumeSchemaClass } from 'src/entities/resume.schema';
 import {
   ACApplicationRepository,
   ApplicationRepository,
@@ -17,24 +19,37 @@ import {
   ACJobPostingRepository,
   JobPostingRepository,
 } from 'src/repositories/job-posting.repository';
+import {
+  ACResumeRepository,
+  ResumeRepository,
+} from 'src/repositories/resume.repository';
+import { CloudinaryService } from 'src/services/cloudinary.service';
+import { JobApplicationService } from 'src/services/job-application.service';
 import { JobPostingService } from 'src/services/job-posting.service';
 import { CompanyModule } from './company.module';
+import { EmailModule } from './email.module';
 
 @Module({
   imports: [
     CompanyModule,
+    EmailModule,
     MongooseModule.forFeature([
       { name: JobPostingSchemaClass.name, schema: JobPostingSchema },
       { name: ApplicationSchemaClass.name, schema: ApplicationSchema },
+      { name: ResumeSchemaClass.name, schema: ResumeSchema },
     ]),
   ],
-  controllers: [JobPostingController],
+  controllers: [JobPostingController, JobApplicationController],
   providers: [
     JobPostingService,
+    JobApplicationService,
+    CloudinaryService,
     JobPostingRepository,
     ApplicationRepository,
+    ResumeRepository,
     { provide: ACJobPostingRepository, useClass: JobPostingRepository },
     { provide: ACApplicationRepository, useClass: ApplicationRepository },
+    { provide: ACResumeRepository, useClass: ResumeRepository },
   ],
 })
 export class JobPostingModule {}
