1b12a16a0003ca243724b34f03be0a764a62de78
Trishan Wagle <mailtotrishan@gmail.com>
2026-02-21 10:05:00 +0545

    feat: wire resume repository bindings into user module

--- kaarya_backend/src/modules/user.module.ts
+++ kaarya_backend/src/modules/user.module.ts
@@ -11,6 +11,10 @@ import {
 } from 'src/entities/gamification-profile.schema';
 import { UserSchemaClass, UserSchema } from 'src/entities/user.schema';
 import {
+  ResumeSchemaClass,
+  ResumeSchema,
+} from 'src/entities/resume.schema';
+import {
   ACAuthIdentityRepository,
   AuthIdentityRepository,
 } from 'src/repositories/auth-identity.repository';
@@ -18,6 +22,10 @@ import {
   ACUserRepository,
   UserRepository,
 } from 'src/repositories/user.repository';
+import {
+  ACResumeRepository,
+  ResumeRepository,
+} from 'src/repositories/resume.repository';
 import { AdminUserService } from 'src/services/admin/admin.user.service';
 import { CloudinaryService } from 'src/services/cloudinary.service';
 import { UserService } from 'src/services/user.service';
@@ -31,6 +39,7 @@ import { UserService } from 'src/services/user.service';
         schema: GamificationProfileSchema,
       },
       { name: AuthIdentitySchema.name, schema: AuthIdentitySchemaModel },
+      { name: ResumeSchemaClass.name, schema: ResumeSchema },
     ]),
   ],
   controllers: [AdminUserController],
@@ -40,14 +49,17 @@ import { UserService } from 'src/services/user.service';
     CloudinaryService,
     UserRepository,
     AuthIdentityRepository,
+    ResumeRepository,
     { provide: ACUserRepository, useClass: UserRepository },
     { provide: ACAuthIdentityRepository, useClass: AuthIdentityRepository },
+    { provide: ACResumeRepository, useClass: ResumeRepository },
   ],
   exports: [
     UserService,
     AdminUserService,
     ACUserRepository,
     ACAuthIdentityRepository,
+    ACResumeRepository,
   ],
 })
 export class UserModule {}
